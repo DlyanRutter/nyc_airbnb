57f503b867a8daf3ca70f77ccfce629ffee604b7
Dylan Rutter <dylan.rutter@my.wheaton.edu>
2023-02-06 14:35:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# main pipeline file
+
+# setuptools error can be fixed with conda update --force conda
 
 import mlflow, os, hydra, json
 from omegaconf import DictConfig, OmegaConf
@@ -10,43 +11,47 @@ def go(config: DictConfig):
     os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
 
     # Steps to execute
+    #steps_par = config['main']['steps']
+    #active_steps = steps_par.split(",") if steps_par != "all" else _steps
     root_path = hydra.utils.get_original_cwd()
     if isinstance(config["main"]["execute_steps"], str):
         steps_to_execute = config["main"]["execute_steps"].split(",")
     else:
+        #print (type(config["main"]["execute_steps"]))
+        #assedrt isinstance(config["main"]["execute_steps"], list)
         steps_to_execute = config["main"]["execute_steps"]
 
 
-    if "download" in steps_to_execute:
+    if "download" in steps_to_execute:#active_steps:
         # Download file and load in W&B
         _ = mlflow.run(
             #os.path.join(root_path, "download"),
-            f"{config['main']['components_repository']}/get_data",
+            config['main']['download_repository'], ##f"{config['main']['components_repository']}/get_data",
             version="main",
             entry_point="main",
             parameters={
-                    "sample": config["data"]["sample"], #"file_url": config["data"]#["file_url"], #taken from hydra
-                    "artifact_name": "sample.csv",
-                    "artifact_type": "raw_data",
-                    "artifact_description": "Raw file as downloaded"
+                    "file_url": config["data"]["file_url"], 
+                    "artifact_name": "raw_data.csv", 
+                    "artifact_type": "raw_data", 
+                    "artifact_description": "Raw file as downloaded" 
                 },
             )
 
-    if "preprocess" in steps_to_execute:
-        # minor preprocessing
+    if "basic_cleaning" in steps_to_execute:
+
         _ = mlflow.run(
-            os.path.join(root_path, "preprocess"),
+            os.path.join(root_path, "basic_cleaning"),
             entry_point="main",
             parameters={
-                "input_artifact": "sample.csv:latest",
-                "artifact_name": "preprocessed_data.csv",
-                "artifact_type": "preprocessed_data",
-                "artifact_description": "Data with preprocessing applied",
+                "input_artifact": "raw_data.csv:latest",
+                "output_name": "preprocessed_data.csv", #was artifact_name
+                "output_type": "preprocessed_data", # was artifact_type
+                "output_description": "Data with preprocessing applied", #was artifact description
                 "min_price": config["data"]["min_price"],
                 "max_price": config["data"]["max_price"]})
 
     if "check_data" in steps_to_execute:
-        # tests to confirm data is valid
+
         _ = mlflow.run(
             os.path.join(root_path, "check_data"),
             entry_point="main",
@@ -57,8 +62,12 @@ def go(config: DictConfig):
                 "min_price": config["data"]["min_price"],
                 "max_price": config["data"]["max_price"]})
 
+    if "EDA" in steps_to_execute:
+        _ = mlflow.run(
+            os.path.join(root_path, "EDA"),
+            entry_point="main")
+
     if "segregate" in steps_to_execute:
-        # split into train and test sets
         _ = mlflow.run(
             os.path.join(root_path, "segregate"),
             entry_point="main",
@@ -71,7 +80,7 @@ def go(config: DictConfig):
             })
 
     if "random_forest" in steps_to_execute:
-        # train model
+
         # NOTE: we need to serialize the random forest configuration into JSON
         model_config=os.path.abspath("random_forest_config.yml")
         rf_config = os.path.abspath("rf_config.json")
@@ -91,8 +100,9 @@ def go(config: DictConfig):
                 "stratify_by": config["data"]["stratify_by"],
                 "max_tfidf_features": config["random_forest_pipeline"]["max_tfidf_features"]})
             
+
+
     if "test_regression_model" in steps_to_execute:
-        # run model on test set
         _ = mlflow.run(
             os.path.join(root_path, "test_regression_model"),
             entry_point="main",
